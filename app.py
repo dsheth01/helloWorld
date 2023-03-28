57aa97eae7dd52744622f2a48f7b474b4b2a63d4
Dev Sheth <dsheth@terpmail.umd.edu>
2023-03-28 14:34:26 -0400

    Created favorite_course.html, contact.html, and base.html routes and templates. Linked bootstrap to base and extended base to child templates.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,5 +15,28 @@ def hello():  # put application's code here
 def about():  # put application's code here
     return render_template('about.html')
 
+@app.route('/favorite_course')
+def favorite_course():
+    print('Subject: ' + request.args.get('subject'))
+    print('Course Number: ' + request.args.get('course_number'))
+    return render_template('favorite_course.html')
+
+@app.route('/contact', methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run()
+
+
+'''     print('First Name entered: ' + request.args.get('first_name'))
+        print('Last Name entered: ' + request.args.get('last_name'))
+        print('Email entered: ' + request.args.get('email'))
+        print('Phone Number entered: ' + request.args.get('phone'))
+
+        if request.get.form('agree_check'):
+            print('Agree to be contacted entered' + request.args.get('phone'))'''
